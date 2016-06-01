83fbb90e0fbf22047eaf21e4b61aff89b5376188
WizardMarcos <wizardmarcos@gmail.com>
2016-06-01 13:26:43 +0100

    Level Class

---
+++ Electroman/Level.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Electroman
+{
+    class Level
+    {
+        public LevelShape Shape;
+        public LevelType Type;
+        public Vector2 Position;
+
+        public Level(LevelShape shape, LevelType type)
+        {
+            Shape = shape;
+            Type = type;
+            Position = new Vector2(0, 0);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+
+            spriteBatch.End();
+        }
+    }
+}
--- Electroman/Misc.cs
+++ Electroman/Misc.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 namespace Electroman
 {
     public enum MagnetDirection { Up = -1, Down = 1 };
+    public enum LevelShape { Straight = 0 };
+    public enum LevelType { Normal = 0 };
 
     class Misc
     {
